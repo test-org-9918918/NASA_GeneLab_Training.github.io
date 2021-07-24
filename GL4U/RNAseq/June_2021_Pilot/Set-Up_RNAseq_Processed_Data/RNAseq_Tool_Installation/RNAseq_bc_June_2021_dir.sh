c40ba4a4b37e71c5fe3b8a1cac56ace8f9852bb2
asaravia-butler <70983120+asaravia-butler@users.noreply.github.com>
2021-07-23 22:27:37 -0700

    Update RNAseq_bc_June_2021_dir.sh

--- GL4U/RNAseq/June_2021_Pilot/Set-Up_RNAseq_Processed_Data/RNAseq_Tool_Installation/RNAseq_bc_June_2021_dir.sh
+++ GL4U/RNAseq/June_2021_Pilot/Set-Up_RNAseq_Processed_Data/RNAseq_Tool_Installation/RNAseq_bc_June_2021_dir.sh
@@ -1,23 +1,40 @@
-mkdir 00-RawData  01-TG_Preproc  02-STAR_Alignment  03-RSEM_Counts  04-DESeq2_NormCounts  05-DESeq2_DGE  Metadata processing_scripts
-mkdir 00-RawData/Fastq
-mkdir 00-RawData/FastQC_Reports
-mkdir 00-RawData/FastQC_Reports/raw_multiqc_report
+mkdir GLDS-104
 
-mkdir 01-TG_Preproc/Fastq
-mkdir 01-TG_Preproc/FastQC_Reports
-mkdir 01-TG_Preproc/FastQC_Reports/trimmed_multiqc_report
-mkdir 01-TG_Preproc/Trimming_Reports
+mkdir GLDS-104/00-RawData  01-TG_Preproc  02-STAR_Alignment  03-RSEM_Counts  04-DESeq2_NormCounts  05-DESeq2_DGE  Metadata processing_scripts
+mkdir GLDS-104/00-RawData/Fastq
+mkdir GLDS-104/00-RawData/FastQC_Reports
+mkdir GLDS-104/00-RawData/FastQC_Reports/raw_multiqc_report
 
-mkdir 05-DESeq2_DGE/ERCC_NormDGE
+mkdir GLDS-104/01-TG_Preproc
+mkdir GLDS-104/01-TG_Preproc/Fastq
+mkdir GLDS-104/01-TG_Preproc/FastQC_Reports
+mkdir GLDS-104/01-TG_Preproc/FastQC_Reports/trimmed_multiqc_report
+mkdir GLDS-104/01-TG_Preproc/Trimming_Reports
 
-mkdir processing_scripts/00-RawData
-mkdir processing_scripts/01-TG_Preproc
-mkdir processing_scripts/02-STAR_Alignment
-mkdir processing_scripts/03-RSEM_Counts
-mkdir processing_scripts/04-05-DESeq2_NormCounts_DGE
+mkdir genomes_gtf
+mkdir genomes_gtf/ensembl_101
+mkdir genomes_gtf/ensembl_101/Mus_musculus
 
-mkdir processing_scripts/00-RawData/raw_fastqc_out_logs
-mkdir processing_scripts/01-TG_Preproc/TG_out_logs
-mkdir processing_scripts/01-TG_Preproc/trimmed_fastqc_out_logs
-mkdir processing_scripts/02-STAR_Alignment/STAR_align_out_logs
-mkdir processing_scripts/03-RSEM_Counts/rsem_count_out_logs
+mkdir STAR_Indices
+mkdir STAR_Indices/ensembl_101
+mkdir STAR_Indices/ensembl_101/Mus_musculus_RL-100
+
+mkdir GLDS-104/02-STAR_Alignment
+mkdir GLDS-104/02-STAR_Alignment/Log_files
+mkdir GLDS-104/02-STAR_Alignment/aligned_multiqc_report
+
+
+
+mkdir GLDS-104/05-DESeq2_DGE/ERCC_NormDGE
+
+mkdir GLDS-104/processing_scripts/00-RawData
+mkdir GLDS-104/processing_scripts/01-TG_Preproc
+mkdir GLDS-104/processing_scripts/02-STAR_Alignment
+mkdir GLDS-104/processing_scripts/03-RSEM_Counts
+mkdir GLDS-104/processing_scripts/04-05-DESeq2_NormCounts_DGE
+
+mkdir GLDS-104/processing_scripts/00-RawData/raw_fastqc_out_logs
+mkdir GLDS-104/processing_scripts/01-TG_Preproc/TG_out_logs
+mkdir GLDS-104/processing_scripts/01-TG_Preproc/trimmed_fastqc_out_logs
+mkdir GLDS-104/processing_scripts/02-STAR_Alignment/STAR_align_out_logs
+mkdir GLDS-104/processing_scripts/03-RSEM_Counts/rsem_count_out_logs
